f62493fbe9d71e24d204d0e1998bdd4d6dcd6303
Eugene Kyalo <yovlinpentdragonvii@gmail.com>
2024-10-04 10:55:35 +0300

    finished seeding

--- server/models.py
+++ server/models.py
@@ -1,18 +1,23 @@
+# server/models.py
+
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData()
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
-
-
-class Pet(db.Model):
+class Pet(db.Model,):
     __tablename__ = 'pets'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
+    breed = db.Column(db.String)
+    age = db.Column(db.Integer)  # Add this line for 'age'
 
-    def __repr__(self):
-        return f'<Pet {self.id}, {self.name}, {self.species}>'
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'species': self.species,
+            'breed': self.breed,
+            'age': self.age,  # Add this to include 'age' in the response
+        }
